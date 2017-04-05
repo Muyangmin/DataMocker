7980785ee694374e072e1035d7b29f18cc3eed32
Muyangmin <muyangmin@foxmail.com>
2017-04-05 18:09:35 +0800

    update comments for library classes.

--- data-mocker/src/main/java/org/mym/datamocker/rule/Rule.java
+++ data-mocker/src/main/java/org/mym/datamocker/rule/Rule.java
@@ -1,23 +1,23 @@
-package org.mym.datamocker.rule;
-
-/**
- * Copyright 2017 Muyangmin
- * <p>
+/*
+ * Copyright (C) 2017 Muyangmin
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * <p>
- * <p>
+ */
+package org.mym.datamocker.rule;
+
+/**
  * An entity class for constraint rules.
- * <p>
+ *
  * Created by Muyangmin on 3/27/17.
  */
 public class Rule {
